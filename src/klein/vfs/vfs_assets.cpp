935fb8f7da32fcee70a6318fbd394ad5faaf3d00
griffi-gh <prasol258@gmail.com>
2026-05-11 21:14:18 +0200

    fix relative path loading

--- src/klein/vfs/vfs_assets.cpp
+++ src/klein/vfs/vfs_assets.cpp
@@ -5,7 +5,9 @@
 
 namespace klein::vfs {
     std::filesystem::path resolve_asset_path(const std::filesystem::path& asset) {
-        static const std::filesystem::path assets_path = ASSETS_PATH;
+        std::filesystem::path assets_path = ASSETS_PATH;
+        if (assets_path.is_relative())
+            assets_path = std::filesystem::absolute(assets_path);
         auto resolved = (assets_path / asset).lexically_normal();
         spdlog::debug("asset path resolved: {} -> {}",
             asset.string(), resolved.string());
